fb9ff684a1577c18c1af9495d4858157f2022b12
Dijash <utsavbhattarai29@gmail.com>
2026-07-30 21:49:21 +0545

    Nav Bar Completed

--- core/views.py
+++ core/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from .models import HeroBanner
 
 def index(request):
-    return render(request, 'core/index.html')
+    hero_banners = HeroBanner.objects.filter(is_active=True).order_by('order')
+    return render(request, 'core/index.html', {
+        'hero_banners': hero_banners
+    })
 
 def contact(request):
     return render(request, 'core/contact.html')
