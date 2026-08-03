8c2e49edac7faec9e87f7c2976b3ebeefc74061b
Dijash <utsavbhattarai29@gmail.com>
2026-08-03 12:10:43 +0545

    OAuth Added Successfully

--- accounts/models.py
+++ accounts/models.py
@@ -7,8 +7,23 @@ class Profile(models.Model):
     phone = models.CharField(max_length=20, blank=True, default='')
     address = models.TextField(blank=True, default='')
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    avatar_url = models.URLField(max_length=500, blank=True, default='')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Profile of {self.user.username}"
+
+    @property
+    def profile_picture(self):
+        if self.avatar:
+            return self.avatar.url
+        if self.avatar_url:
+            return self.avatar_url
+        try:
+            social_acc = self.user.socialaccount_set.first()
+            if social_acc:
+                return social_acc.get_avatar_url() or social_acc.extra_data.get('picture', '')
+        except Exception:
+            pass
+        return ''
--- accounts/signals.py
+++ accounts/signals.py
@@ -1,6 +1,9 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
+from allauth.account.signals import user_logged_in, user_signed_up
+from allauth.socialaccount.signals import pre_social_login
+from allauth.socialaccount.models import SocialAccount
 from .models import Profile
 
 
@@ -12,3 +15,28 @@ def create_or_update_profile(sender, instance, created, **kwargs):
         if not hasattr(instance, 'profile'):
             Profile.objects.create(user=instance)
         instance.profile.save()
+
+
+@receiver(user_logged_in)
+@receiver(user_signed_up)
+@receiver(pre_social_login)
+def sync_social_profile_picture(sender, request=None, user=None, sociallogin=None, **kwargs):
+    target_user = user or (sociallogin.user if sociallogin else None)
+    if not target_user or not target_user.pk:
+        return
+    
+    if not hasattr(target_user, 'profile'):
+        Profile.objects.create(user=target_user)
+        
+    try:
+        social_acc = SocialAccount.objects.filter(user=target_user).first()
+        if not social_acc and sociallogin:
+            social_acc = sociallogin.account
+
+        if social_acc and social_acc.extra_data:
+            picture_url = social_acc.extra_data.get('picture') or social_acc.get_avatar_url()
+            if picture_url and target_user.profile.avatar_url != picture_url:
+                target_user.profile.avatar_url = picture_url
+                target_user.profile.save()
+    except Exception:
+        pass
