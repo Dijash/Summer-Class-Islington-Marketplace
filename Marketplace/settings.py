964fd2f85dadb48a22f3fffc85af8d6b120feb69
Dijash <utsavbhattarai29@gmail.com>
2026-08-04 16:22:54 +0545

    Hot Fix

--- Marketplace/settings.py
+++ Marketplace/settings.py
@@ -77,10 +77,7 @@ WSGI_APPLICATION = 'Marketplace.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=config('DATABASE_URL'), conn_max_age=600)
 }
 
 
