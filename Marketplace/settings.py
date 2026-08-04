e116c053c40f2966da06a0154a138a161fe2f502
Dijash <utsavbhattarai29@gmail.com>
2026-08-04 16:51:22 +0545

    Deployment Push Final

--- Marketplace/settings.py
+++ Marketplace/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/staticfiles/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
@@ -173,5 +173,5 @@ LOGOUT_REDIRECT_URL = '/'
 ACCOUNT_ADAPTER = 'accounts.adapters.CustomAccountAdapter'
 SOCIALACCOUNT_ADAPTER = 'accounts.adapters.CustomSocialAccountAdapter'
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
