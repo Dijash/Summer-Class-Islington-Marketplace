92f81bd8d16a11540ec60029d96d68932e38a5db
Dijash <utsavbhattarai29@gmail.com>
2026-08-05 15:55:12 +0545

    Setting Fixes

--- Marketplace/settings.py
+++ Marketplace/settings.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 # pyrefly: ignore [missing-import]
+import dj_database_url
+# pyrefly: ignore [missing-import]
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -11,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6x!w@s_dk4&b^#@7+z82%yzl(mqs2ro#!=2t*-ms3o9!ggrv+s'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-6x!w@s_dk4&b^#@7+z82%yzl(mqs2ro#!=2t*-ms3o9!ggrv+s')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
@@ -78,12 +80,18 @@ WSGI_APPLICATION = 'Marketplace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# 1. This acts as your local default
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# 2. This safely overrides it ONLY if DATABASE_URL is present in the environment (e.g., on Render)
+database_url = config('DATABASE_URL', default=None)
+if database_url:
+    DATABASES['default'] = dj_database_url.parse(database_url, conn_max_age=600)
 
 
 # Password validation
